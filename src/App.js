df7574e761bd927e78047c703f51cd908ce0d681
Aly Mohamed <alyabdulmottalib@gmail.com>
2022-07-28 01:03:30 +0200

    Adds Debug function for notes, also defines message class

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ firebase.initializeApp({
 
 const auth = firebase.auth()
 const firestore = firebase.firestore()
+const DBmessages = firestore.collection('messages') // refers to collection of messages in firebase, can be queried, removed from and added into
+
 // check https://www.pluralsight.com/guides/setting-up-a-react-project-from-github on how to run this and install necessary files in node_modules
 function App() {
   const [user] = useAuthState(auth)
@@ -32,9 +34,8 @@ function App() {
         </section>
       </header>
     </div>
-  );
+  )
 }
-
 function SignIn() {
   const signInWithGoogle = () =>{
     const provider = new firebase.auth.GoogleAuthProvider()
@@ -42,5 +43,42 @@ function SignIn() {
   }
   return <button onClick={signInWithGoogle}>sign in with google</button>
 }
-export default App;
+function SignOut() {
+  return <button onClick={()=> auth.signOut()}>Sign out</button>
+}
+function DebugFunction() {//function for notes, to be deleted later, not a comment to provide highlighting
 
+  //how to get user auth info: referenced from https://firebase.google.com/docs/auth/web/manage-users#web-version-8_1
+  if (auth.currentUser() !== null) {
+      auth.currentUser().providerData.forEach((user) => { //use auth.currentUser().member to get value of member,
+      console.log("Sign-in provider: " + user.providerId);
+      console.log("  Provider-specific UID: " + user.uid);// auth.currentUser().uid returns same result
+      console.log("  Name: " + user.displayName);
+      console.log("  Email: " + user.email);
+      console.log("  Photo URL: " + user.photoURL); //photo associated with email
+    });
+  }
+  console.log(firebase.database.ServerValue.TIMESTAMP) //returns current date, never use any other js time function to get time
+}
+class message{ //class to hold information on messages, should be a prop in the ' Message ' react function component
+  constructor(content, uid, name){
+    this.content = content
+    this.uid = uid
+    this.name = name
+  }
+}
+async function sendMessageToDB(message){ //should add message to firestore
+  const uid = auth.currentUser().uid
+  const photoURL = auth.currentUser().photoURL
+  try {
+    await DBmessages.add({
+      text: message.content,
+      date: firebase.firestore.FieldValue.serverTimestamp(),
+      uid: uid,
+      photo: photoURL
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+export default App;
