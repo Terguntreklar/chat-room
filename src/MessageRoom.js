d36c18450d285822dc5365d156b0db8b0cd93827
Aly Mohamed <69851232+Terguntreklar@users.noreply.github.com>
2022-08-03 11:38:12 +0200

    Revert "uploading updates"

--- src/MessageRoom.js
+++ src/MessageRoom.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react'
 import { firestore, auth, firebase,storage } from './firebase'
 import { useCollectionData } from 'react-firebase-hooks/firestore'
-import SendIcon from '@mui/icons-material/Send';
+import { SignOut } from './SignOut'
 
 const DBmessages = firestore.collection('messages')
 const query = DBmessages.orderBy('timestamp').limit(50)
@@ -19,9 +19,8 @@ const converter = {
         }
     }
 }
-
-const month = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 const imagesRef = storage.ref()
+
 export default function MessageRoom() {
     
     const [messages] = useCollectionData(query.withConverter(converter))
@@ -32,9 +31,6 @@ export default function MessageRoom() {
     const scrollToBottom = () => {
         scrolldiv.current.scrollIntoView({ behavior: "smooth" })
     }
-    useEffect(scrollToBottom, [messages]);
-    const [formValue, setFormValue] = useState('')
-    const sendMessageToDB = async(e)=>{
     useEffect(scrollToBottom, [messages])
     const imageHandler = async(e) =>{
         window.alert("image uploading, please wait for confirmation, I don't know how to make a progressbar 😒")
@@ -63,30 +59,12 @@ export default function MessageRoom() {
         const uid = auth.currentUser.uid
         const photoURL = auth.currentUser.photoURL
         const displayName = auth.currentUser.displayName
-        if (formValue === '') {
-            return;
-        }
         await DBmessages.add({
             text: formValue,
             displayName: displayName,
             timestamp: firebase.firestore.FieldValue.serverTimestamp(),
             uid: uid,
             photoURL: photoURL,
-        });
-        setFormValue('');
-        scrollToBottom();
-    }
-    return (
-        <section className='msg-sec'>
-            <div className='msg-cont'>
-                {messages && messages.map((message)=><Message key = {message.id} message = {message}/>)}
-            </div>
-            <div className='msg-form'>
-                <form onSubmit={sendMessageToDB}>
-                    <input value={formValue} placeholder={'enter message'} onChange={(e) => setFormValue(e.target.value)}/>
-                    <button className='send-btn' type='submit'> <SendIcon /> </button>
-                </form>
-            </div>
             downloadURL: imageURL
         })
         setFormValue('')
@@ -111,17 +89,10 @@ export default function MessageRoom() {
             </form>
           <div ref={scrolldiv}></div>
         </section>
+        </>
     )
 }
 function Message(props){
-    const { text,displayName, timestamp, uid, photoURL } = props.message
-    let d = ''
-    if (timestamp == null) {
-        d = 'just now'
-    }
-    else{
-        d =`${timestamp.toDate().getDate()}/${month[timestamp.toDate().getMonth()]}`;
-    }
     const { text,displayName, timestamp, uid, photoURL, downloadURL} = props.message
     let d = timestamp==null?'just now':`${timestamp.toDate().getDate()}/${timestamp.toDate().getMonth()}`
     let imgDisplay = (downloadURL==="")?{display: "none"}:{"":""} //move this to some css file
