f9f4450fec30e366702e1bfa7d8edf0b6c5d3462
Aly Mohamed <alyabdulmottalib@gmail.com>
2022-08-04 20:01:13 +0200

    adds css

--- src/App.js
+++ src/App.js
@@ -4,23 +4,32 @@ import React, { useRef } from 'react'
 import { firebase,auth } from "./firebase";
 import { SignIn } from './SignIn';
 import { useAuthState } from 'react-firebase-hooks/auth'
-
+import MessageIcon from '@mui/icons-material/Message';
+import { pink } from '@mui/material/colors';
 
 import MessageRoom from './MessageRoom';
-
+import { SignOut } from './SignOut';
 // check https://www.pluralsight.com/guides/setting-up-a-react-project-from-github on how to run this and install necessary files in node_modules
 function App() {
   const [user] = useAuthState(auth)
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <section>
-          {user?<MessageRoom/>:<SignIn/>} {/* replace null here with the hompage rfc */}
-        </section>
+        <h1 className='main-title'>Chat Room</h1>
+        {user ? <SignOut /> : <MessageIcon sx={{ color: pink[500], fontSize: 40 }} />}
       </header>
+      <main>
+        {
+          user ?
+          <MessageRoom /> :
+          <div className='signin-cont'>
+            <SignIn />
+          </div>
+        }
+      </main>
     </div>
-  )
+  );
 }
 
 function DebugFunction() {//function for notes, to be deleted later, not a comment to provide highlighting
