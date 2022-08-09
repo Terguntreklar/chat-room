92c059b9436f012d7dcc94d55a583585329fc5ad
Aly Mohamed <69851232+Terguntreklar@users.noreply.github.com>
2022-08-10 01:15:35 +0200

    final touches pt.3 (#6)

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,7 @@
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
-import "firebase/compat/storage";
-
+import 'firebase/compat/storage';
 firebase.initializeApp({
     apiKey: "AIzaSyCcOH31ooiSwNY7bQUbVUdliPWm73ys_Rg",
     authDomain: "chat-room-efca6.firebaseapp.com",
